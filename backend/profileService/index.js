b50c34639b45349ac3ff3a3a5cb8a40d2274afbf
ankitanand200193 <ankit200193@gmail.com>
2025-07-13 17:24:03 +0530

    Update index.js

--- backend/profileService/index.js
+++ backend/profileService/index.js
@@ -13,7 +13,7 @@ mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("✅ MongoDB connected successfully"))
+.then(() => console.log(" MongoDB connected successfully"))
 .catch((err) => {
   console.error("❌ MongoDB connection error:", err);
   process.exit(1); // Exit if MongoDB is not reachable
